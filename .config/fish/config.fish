b374a00f724c71373eb3508af94228a37ecfd3ad
Vu Le <thenewvu@gmail.com>
2016-09-06 21:07:42 +0700

    [fish] Started X at login with fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,14 @@
+# fix locale issue in the recent fish version
 if not set -q LANG >/dev/null
   set -gx LANG en_US.UTF-8
 end
 
+# add global node module binary dir to PATH
 set -gx PATH /home/vu/.node_modules/bin $PATH
+
+# start X at login
+if status --is-login
+  if test -z "$DISPLAY" -a "$XDG_VTNR" -eq "1"
+    exec startx -- -keeptty
+  end
+end
