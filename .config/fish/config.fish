8126c53b862f38bda0386c44dc391b28d5beb27e
Vu Le <thenewvu@gmail.com>
2016-09-20 09:14:12 +0700

    [fish] Added VISUAL envvar

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,9 @@ end
 # additional bin paths
 set -gx PATH /home/vu/.node_modules/bin /home/vu/.bin $PATH
 
+# default editor
+set -gx VISUAL="vim"
+
 # start X at login
 if status --is-login
   if test -z "$DISPLAY" -a "$XDG_VTNR" -eq "1"
