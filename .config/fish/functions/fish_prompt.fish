c9b2256ae826bd11b88ee59178875ca0781e66a1
Vu Le <thenewvu@gmail.com>
2016-10-23 14:46:33 +0700

    [fish] Fixed unknown characters in prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -15,46 +15,46 @@ function fish_prompt
 
   echo -n (set_color $red)(whoami)(set_color $green)'@'(set_color $yellow)(hostname)':'
   test $USER = 'root'; and echo (set_color $red)"#"
-  echo -n (set_color $blue)(prompt_pwd) (set_color $red)'❯'(set_color $yellow)'❯'(set_color $green)'❯ '
+  echo -n (set_color $blue)(prompt_pwd) (set_color $red)'⟩'(set_color $yellow)'⟩'(set_color $green)'⟩ '
 end
 
 function fish_right_prompt
-    # last status
-    test $status != 0; and printf (set_color red)"⏎ "
+  # last status
+  test $status != 0; and printf (set_color red)"⏎ "
 
   if git rev-parse ^ /dev/null
-        # Purple if branch detached else green
-        git branch -qv | grep "\*" | grep -q detached
-            and set_color purple --bold
-            or set_color green --bold
+    # Purple if branch detached else green
+    git branch -qv | grep "\*" | grep -q detached
+        and set_color purple --bold
+        or set_color green --bold
 
-        # Need optimization on this block (eliminate space)
-        git name-rev --name-only HEAD
+    # Need optimization on this block (eliminate space)
+    git name-rev --name-only HEAD
 
-        # Merging state
-        git merge -q ^ /dev/null; or printf ':'(set_color red)'merge'
-        printf ' '
+    # Merging state
+    git merge -q ^ /dev/null; or printf ':'(set_color red)'merge'
+    printf ' '
 
-        # Symbols
+    # Symbols
     for i in (git branch -qv --no-color|grep \*|cut -d' ' -f4-|cut -d] -f1|tr , \n)\
             (git status --porcelain | cut -c 1-2 | uniq)
       switch $i
-                case "*[ahead *"
-                    printf (set_color purple)⬆' '
-                case "*behind *"
-                    printf (set_color purple)⬇' '
+        case "*[ahead *"
+            printf (set_color purple)↑' '
+        case "*behind *"
+            printf (set_color purple)↓' '
         case "."
-          printf (set_color green)✚' '
+          printf (set_color green)a' '
         case " D"
-          printf (set_color red)✖' '
+          printf (set_color red)d' '
         case "*M*"
-          printf (set_color blue)✱' '
-                case "*R*"
-                    printf (set_color purple)➜' '
-                case "*U*"
-                    printf (set_color brown)═' '
+          printf (set_color blue)m' '
+        case "*R*"
+            printf (set_color purple)r' '
+        case "*U*"
+            printf (set_color brown)u' '
         case "??"
-          printf (set_color white)◼' '
+          printf (set_color white)?' '
       end
     end
   end
