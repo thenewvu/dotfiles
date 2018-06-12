e353be7d322fb0226b2d014a9f63af8df4458e1f
Vu Le <thenewvu@gmail.com>
2018-06-12 08:40:59 +0700

    [bitbar] fix task command not found

--- .config/bitbar/plugins/taskwarrior.5m.sh
+++ .config/bitbar/plugins/taskwarrior.5m.sh
@@ -6,20 +6,21 @@
 #
 
 style="color=#af51ff font='Script12 BT' size=12"
-started=$(task +ACTIVE _id | head -n 1)
-stopped=$(task -ACTIVE _id)
+task=/usr/local/bin/task
+started=$($task +ACTIVE _id | head -n 1)
+stopped=$($task -ACTIVE _id)
 
 if [ "$started" != "" ]; then
-    proj=$(task _get $started.project)
+    proj=$($task _get $started.project)
     if [ "$proj" != "" ]; then
         proj="$proj: "
     fi
 
-    desc=$(task _get $started.description)
+    desc=$($task _get $started.description)
 
     opts="terminal=false refresh=true \
         bash=/bin/bash param1=-c \
-        param2='task stop $started'"
+        param2='$task stop $started'"
     echo "$proj$desc | $style $opts"
 else
     echo "$(echo "$stopped" | wc -l) tasks | $style"
@@ -28,22 +29,22 @@ fi
 echo "---"
 
 for id in $stopped; do
-    proj=$(task _get $id.project)
+    proj=$($task _get $id.project)
     if [ "$proj" != "" ]; then
         proj="$proj: "
     fi
 
-    desc=$(task _get $id.description)
+    desc=$($task _get $id.description)
 
     opts="terminal=false refresh=true \
         bash=/bin/bash param1=-c \
-        param2='task start $id'"
+        param2='$task start $id'"
 
     echo "$proj$desc | $opts $style"
 
     opts="alternate=true terminal=false \
         refresh=true bash=/bin/bash param1=-c \
-        param2='task done $id'"
+        param2='$task done $id'"
 
     echo "$proj$desc | $opts $style"
 done
