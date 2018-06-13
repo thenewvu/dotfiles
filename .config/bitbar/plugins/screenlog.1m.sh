2b6846aceeceb946e1afabdd26955bb097687898
Vu Le <thenewvu@gmail.com>
2018-06-14 01:04:56 +0700

    [bibar] screenlog - support low/high quality

--- .config/bitbar/plugins/screenlog.1m.sh
+++ .config/bitbar/plugins/screenlog.1m.sh
@@ -8,7 +8,8 @@
 pidf=/tmp/com.bitbar.screenlog.pid
 logf=/tmp/com.bitbar.screenlog.log
 
-ffmpeg="/usr/local/bin/ffmpeg -f avfoundation -framerate 1 -pixel_format bgr0 -i 1 -c:v libvpx-vp9 -cpu-used 8 -deadline realtime -crf 50 -b:v 256K"
+ffmpeg_fastest="/usr/local/bin/ffmpeg -f avfoundation -framerate 1 -pixel_format bgr0 -i 1 -c:v libvpx-vp9 -cpu-used 8 -deadline realtime -crf 50 -b:v 256K"
+ffmpeg_slowest="/usr/local/bin/ffmpeg -f avfoundation -framerate 1 -pixel_format bgr0 -i 1 -c:v libvpx-vp9 -cpu-used 8 -deadline realtime -crf 31 -b:v 0"
 
 outdir=~/Documents/screenlogs
 
@@ -42,12 +43,19 @@ if [ "$cmd" == "status" ]; then
     else
         echo "◉  ▿ "
         echo "---"
-        echo "Start | terminal=false refresh=true bash=$0 param1=start"
+        echo "Start (Lo quality) | terminal=false refresh=true bash=$0 param1=start-fastest font='Script12 BT'"
+        echo "---"
+        echo "Start (Hi quality) | terminal=false refresh=true bash=$0 param1=start-slowest font='Script12 BT'"
     fi
 fi
 
-if [ "$cmd" == "start" ]; then
-    nohup $ffmpeg $outdir/$(date +%Y-%m-%d-%H-%M.webm) &
+if [ "$cmd" == "start-fastest" ]; then
+    nohup $ffmpeg_fastest $outdir/$(date +%Y-%m-%d-%H-%M.webm) &
+    echo $! > $pidf
+fi
+
+if [ "$cmd" == "start-slowest" ]; then
+    nohup $ffmpeg_slowest $outdir/$(date +%Y-%m-%d-%H-%M.webm) &
     echo $! > $pidf
 fi
 
